66442f73b1379e43ed47c8b2a86a6f5c94738a20
Rana Dutta <duttarana189@gmail.com>
2025-06-21 23:17:29 +0530

    env updated

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:5000",
+    baseURL: 'https://lms-backend-three-indol.vercel.app' || "http://localhost:5000",
 });
 
 axiosInstance.interceptors.request.use((config) => {
